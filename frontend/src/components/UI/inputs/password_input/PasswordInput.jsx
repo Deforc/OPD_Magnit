2ae6cac754686fd8a714e9a658aa8f4c86295881
Anton Konopelkin <anti.kon.40@yandex.ru>
2023-03-15 20:27:10 +0300

    Добавить функциональность компоненту PasswordInput
    В данном коммите, компоненту PasswordInput добавлена следующая
    функциональность:
    
    - Для условного рендеринга, компоненту PasswordInput добавлено состояние
    (с использованием хука useState) value и функция для его изменения
    setValue, данное состояние связанно со значением value поля <input>.
    
    - Для реализации возможности просмотра введенного пароля, компоненту
    PasswordInput добавлено состояние (с использованием хука useState)
    isVisible и функция для его изменения toggleVisible.
    
    - Для обновления значения состояния value, полю <input> добавлен обработчик
    события изменения значения (onChange) handleChange, устанавливающий в
    качестве значения состояние value, значение value поля <input>.
    
    - Для сокрытия или отображения информации, введённой в поле <input>,
    добавлено условие если состояние isVisible является true, то поле <input>
    в качестве аргумента type принимает значение "text", иначе значение
    "password".
    
    - Для обновления значения состояния value, полю <input> добавлен обработчик
    события изменения значения (onChange) handleChange, устанавливающий в
    качестве значения состояние value, значение value поля <input>.
    
    - Для решения задачи по отображению <button>, добавлено условное
    редактирование (если состояние value не является пустой строкой, то
    <button> отображается).
    
    - Для изменения состояния isVisible <button> добавлен обработчик события
    нажатия (onClick) changeVisible, меняющий значение состояния isVisible на
    противоположное.

--- frontend/src/components/UI/inputs/password_input/PasswordInput.jsx
+++ frontend/src/components/UI/inputs/password_input/PasswordInput.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const PasswordInput = (props) => {
+    const [value, setValue] = useState("");
+    const [isVisible, toggleVisible] = useState(false);
+
+    function handleChange(event) {
+        setValue(event.target.value);
+    }
+
+    function changeVisible() {
+        toggleVisible(!isVisible);
+    }
+
     return (
         <div>
-            <input type="password" placeholder={props.example}></input>
+            <input type={isVisible ? "text" : "password"} placeholder={props.example}
+                   value={value} onChange={handleChange}></input>
             <label>{props.label}</label>
-            <button></button>
+            {(value !== "") && <button onClick={changeVisible}></button>}
         </div>
     );
 };
