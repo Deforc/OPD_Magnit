657968f763f1f0cd1ae12bbaf60ac97aeebd5de0
Anton Konopelkin <anti.kon.40@yandex.ru>
2023-03-15 16:45:31 +0300

    Добавить функциональность компоненту TextInput
    В данном коммите, компоненту TextInput добавлена следующая
    функциональность:
    
    - Для условного рендеринга, компоненту TextInput добавлено состояние
    (с использованием хука useState) value и функция для его изменения
    setValue, данное состояние связанно со значением value поля <input>.
    
    - Для обновления значения состояния value, полю <input> добавлен обработчик
    события изменения значения (onChange) handleChange, устанавливающий в
    качестве значения состояние value, значение value поля <input>.
    
    - Для решения задач по отображению label, содержащего example, и button,
    добавлено условное редактирование (если состояние value является пустой
    строкой, то отображается label, иначе button).
    
    - Для реализации функции очистки текстового поля, кнопке button добавлен
    обработчик события нажатия (onClick) clearValue. Данная функция
    устанавливает значение состояния value, равное пустой строке.

--- frontend/src/components/UI/inputs/text_input/TextInput.jsx
+++ frontend/src/components/UI/inputs/text_input/TextInput.jsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const TextInput = (props) => {
+    const [value, setValue] = useState("")
+
+    function handleChange(event) {
+        setValue(event.target.value);
+    }
+
+    function clearValue() {
+        setValue("");
+    }
+
     return (
         <div>
-            <input type="text"></input>
+            <input type="text" value={value} onChange={handleChange}></input>
             <label>{props.label}</label>
-            <label>{props.example}</label>
-            <button></button>
+            {value === "" ? <label>{props.example}</label> :
+                            <button onClick={clearValue}></button>}
         </div>
     );
 };
