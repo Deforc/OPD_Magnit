7625d00814bb79fa1726c48a98d8b1fabfdc5df9
PpavelFilippov <Roucreat@yandex.ru>
2023-04-16 16:29:14 +0300

    <token>

---
+++ backend/Backend/WebApi/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+        private readonly string _secretKey = "my_secret_key"; // секретный ключ
+        private readonly string _issuer = "my_issuer"; //  имя издателя токена
+        private readonly int _expirationMinutes = 60; // время жизни в минутах
+
+        [HttpPost]
+        public IActionResult GenerateToken()
+        {
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, "username"), // имя пользователя
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_secretKey);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.AddMinutes(_expirationMinutes),
+                Issuer = _issuer,
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key),
+                                                            SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return Ok(new { token = tokenHandler.WriteToken(token) });
+        }
+    }
+}
--- backend/Backend/WebApi/Program.cs
+++ backend/Backend/WebApi/Program.cs
@@ -23,3 +23,19 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+/*
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+*/
