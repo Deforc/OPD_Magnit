c53f927c240f5f2bc86663a4ab63ca88c4f8d190
Alexey Fedorov <fedorov.alexey.02@gmail.com>
2023-05-14 17:40:37 +0300

    Исправил запуск приложения

--- backend/Backend/WebApi/Program.cs
+++ backend/Backend/WebApi/Program.cs
@@ -28,7 +28,6 @@ namespace WebApi1
                                 "http://www.contoso.com")
                             .AllowAnyHeader()
                             .AllowAnyMethod()
-                            .AllowCredentials()
                             .AllowAnyOrigin();
                     });
                 }
